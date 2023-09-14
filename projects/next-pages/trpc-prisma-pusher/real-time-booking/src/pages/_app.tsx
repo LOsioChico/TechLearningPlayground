89b82666821b8860703b67ac00e145aaeb1adb89
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-14 15:01:56 -0500

    style: format _app.tsx

--- projects/next-pages/trpc-prisma-pusher/real-time-booking/src/pages/_app.tsx
+++ projects/next-pages/trpc-prisma-pusher/real-time-booking/src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import { type AppType } from "next/app";
+import { type AppType } from 'next/app'
+import { Toaster } from 'sonner'
 
-import { api } from "@/utils/api";
+import { api } from '@/utils/api'
 
-import "@/styles/globals.css";
+import '@/styles/globals.css'
+import { Layout } from '@/components/Layout'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+  return (
+    <Layout>
+      <Toaster position='top-center' richColors />
+      <Component {...pageProps} />;
+    </Layout>
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
