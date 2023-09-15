34b8dba054b96150f4d03e35b43e05dfa2381b36
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-15 00:46:25 -0500

    feat: add sidebarLinks constants file

---
+++ projects/next-pages/framer-zustand-vara/luxury-agency/src/constants/sidebarLinks.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+  {
+    href: '/',
+    label: 'Home',
+  },
+  {
+    href: '/about',
+    label: 'About',
+  },
+  {
+    href: '/services',
+    label: 'Services',
+  },
+  {
+    href: '/careers',
+    label: 'Careers',
+  },
+  {
+    href: '/contact',
+    label: 'Contact',
+  },
+]
