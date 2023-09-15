5a4d7f1a63d151309cafe5c6325fc31a9362fcfa
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-14 21:02:51 -0500

    style: update colors in tailwind.config.ts

--- projects/next-app/framer/luxury-agency/tailwind.config.ts
+++ projects/next-app/framer/luxury-agency/tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        dark: '#0E1010',
+        darkLight: '#171919',
+        primary: '#e7fbfc',
       },
     },
   },
