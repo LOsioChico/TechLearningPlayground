2a8099956b81edc69471cde0234fdc2260495a98
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-15 01:57:52 -0500

    style: add scroll down indicator in home page

--- projects/next-pages/framer-zustand-vara/luxury-agency/src/pages/index.tsx
+++ projects/next-pages/framer-zustand-vara/luxury-agency/src/pages/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import ScrollDown from '../../public/ScrollDown'
 
 const HomePage = () => {
-  return <section className='bg-dark h-screen'></section>
+  return (
+    <section className='bg-dark h-screen'>
+      <div className='absolute bottom-4 left-12 flex flex-col items-center justify-center gap-1 opacity-50'>
+        <ScrollDown />
+        <span>Scroll Down</span>
+      </div>
+      <div className='flex h-full flex-col items-center justify-center'></div>
+    </section>
+  )
 }
 
 export default HomePage
