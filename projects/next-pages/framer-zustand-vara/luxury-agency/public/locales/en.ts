d5d6ea5077cf66296581178bcd69ad9657650552
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-16 00:26:59 -0500

    feat: Add English and Spanish translations

---
+++ projects/next-pages/framer-zustand-vara/luxury-agency/public/locales/en.ts
@@ -0,0 +1,11 @@
+type Translations = Record<string, string>
+
+export const en: Translations = {
+  Locale: 'EN',
+  Home: 'Home',
+  About: 'About us',
+  Services: 'Services',
+  Careers: 'Careers',
+  Contact: 'Contact',
+  BecomeAModel: 'Become a model',
+}
---
+++ projects/next-pages/framer-zustand-vara/luxury-agency/public/locales/es.ts
@@ -0,0 +1,11 @@
+type Translations = Record<string, string>
+
+export const es: Translations = {
+  Locale: 'ES',
+  Home: 'Inicio',
+  About: 'Sobre',
+  Services: 'Servicios',
+  Careers: 'Carreras',
+  Contact: 'Contacto',
+  BecomeAModel: 'Conviértete en modelo',
+}
