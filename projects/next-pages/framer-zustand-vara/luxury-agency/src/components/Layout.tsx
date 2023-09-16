c01b3a9b906a35e4dd3f2b84d3c058c0f6ee7872
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-16 00:29:17 -0500

    refactor: Refactor layout component, add language switch button

--- projects/next-pages/framer-zustand-vara/luxury-agency/src/components/Layout.tsx
+++ projects/next-pages/framer-zustand-vara/luxury-agency/src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import { VaraText } from '@/components/VaraText'
 import { VscMenu } from 'react-icons/vsc'
 import { motion } from 'framer-motion'
+import { TbWorld } from 'react-icons/tb'
 
+import { useTranslations } from '@/hooks/useTranslations'
 import { useRouter } from 'next/router'
 import { useSidebarMenuStore } from '@/stores/store'
 import { SidebarMenu } from './SidebarMenu'
+
+import Link from 'next/link'
 
 interface LayoutProps {
   children: React.ReactNode
@@ -12,6 +16,7 @@ interface LayoutProps {
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   const router = useRouter()
+  const { t } = useTranslations()
   const { open } = useSidebarMenuStore()
 
   return (
@@ -27,14 +32,36 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
           >
             <VaraText text='Luxury Agency' />
           </div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.6 }}
-            onClick={open}
-          >
-            <VscMenu className='cursor-pointer duration-300' size={40} />
-          </motion.div>
+          <div className='flex gap-6'>
+            <motion.div
+              className='text-primary flex w-16 cursor-pointer items-center justify-center gap-1 rounded-full'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              <Link
+                href='/'
+                className='flex items-center justify-center gap-1'
+                locale={t.Locale === 'EN' ? 'es' : 'en'}
+              >
+                <TbWorld className='text-3xl' />
+                <span className='font-bold uppercase'>{t.Locale}</span>
+              </Link>
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              onClick={open}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              <VscMenu className='cursor-pointer duration-300' size={40} />
+            </motion.div>
+          </div>
         </div>
       </nav>
       {children}
