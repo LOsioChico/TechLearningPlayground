ad739fefeef5bc8b5c1ce44effc4958f220e8d33
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-19 09:51:08 -0500

    refactor(styles): Move theme files to themes folder

--- projects/next-pages/openjira/pages/_app.tsx
+++ projects/next-pages/openjira/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { ThemeProvider, CssBaseline } from '@mui/material'
-import { darkTheme } from '@/styles'
+import { darkTheme } from '@/themes'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
