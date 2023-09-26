92828d3a51ad60fd9c73af6ff8cc195904ae9750
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-26 06:42:54 -0500

    refactor(TitleBar): Refactor TitleBar component

--- projects/tauri/social-nexus/src/components/TitleBar.tsx
+++ projects/tauri/social-nexus/src/components/TitleBar.tsx
@@ -1,12 +1,65 @@
-export const TitleBar = () => {
+'use client'
+import { Minus, Square, X } from 'lucide-react'
+
+interface TitleBarProps {
+  variant?: 'burple' | 'dark'
+}
+
+export const TitleBar: React.FC<TitleBarProps> = ({ variant = 'dark' }) => {
   return (
-    <div className='flex justify-between'>
-      <div className='pl-2'>SocialNexus</div>
-      <div className='flex'>
-        <button className='px-2 hover:bg-white'>-</button>
-        <button className='px-2 hover:bg-white'>[]</button>
-        <button className='px-2 hover:bg-white'>x</button>
+    <div
+      data-tauri-drag-region
+      className={`flex h-5 items-center justify-between ${colors[variant]}`}
+      onDrag={windowActions.startDrag}
+    >
+      <div className='pl-2 text-xs font-extrabold text-gray-400'>
+        SocialNexus
+      </div>
+      <div className='flex h-full items-center justify-center text-gray-400'>
+        <button
+          className='h-full px-1.5 duration-300 hover:bg-black hover:bg-opacity-10 hover:text-white'
+          onClick={windowActions.minimize}
+        >
+          <Minus size={18} strokeWidth={1.4} />
+        </button>
+        <button
+          className='h-full px-2 duration-300 hover:bg-black hover:bg-opacity-10 hover:text-white'
+          onClick={windowActions.maximize}
+        >
+          <Square size={14} strokeWidth={1.4} />
+        </button>
+
+        <button
+          className='hover:bg-red h-full px-1.5 duration-300 hover:text-white'
+          onClick={windowActions.close}
+        >
+          <X size={18} strokeWidth={1.4} />
+        </button>
       </div>
     </div>
   )
+}
+
+const colors = {
+  burple: 'bg-burple',
+  dark: 'bg-dark',
+}
+
+const windowActions = {
+  minimize: async () => {
+    const { appWindow } = await import('@tauri-apps/api/window')
+    void appWindow.minimize()
+  },
+  maximize: async () => {
+    const { appWindow } = await import('@tauri-apps/api/window')
+    void appWindow.toggleMaximize()
+  },
+  close: async () => {
+    const { appWindow } = await import('@tauri-apps/api/window')
+    void appWindow.close()
+  },
+  startDrag: async () => {
+    const { appWindow } = await import('@tauri-apps/api/window')
+    void appWindow.startDragging()
+  },
 }
