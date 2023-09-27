35e0c48d2da6230812e30dbc9b512bdf7acd99a7
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-27 11:20:59 -0500

    style: Update colors in tailwind.config.js

--- projects/tauri/social-nexus/tailwind.config.js
+++ projects/tauri/social-nexus/tailwind.config.js
@@ -10,12 +10,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        darkGray: '#313338',
         red: '#ED4245',
         green: '#57F287',
         yellow: '#FEE75C',
         fuchsia: '#EB459E',
         burple: '#5865F2',
+        burpleHover: '#4752c4',
+        burpleActive: '#3c45a5',
+        darkGray: '#313338',
+        normalGray: '#B4B4B4',
+        lightGray: '#EDEDED',
+        inputBackground: '#1e1f22',
+        textLink: '#00a8fc',
       },
     },
   },
