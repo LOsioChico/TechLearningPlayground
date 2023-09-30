8c35ea5263f4ee441a69318100540a2ba148096a
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-29 20:10:32 -0500

    feat: Add window shadows feature

--- projects/tauri/social-nexus/src-tauri/src/main.rs
+++ projects/tauri/social-nexus/src-tauri/src/main.rs
@@ -1,8 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::Manager;
+use window_shadows::set_shadow;
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let context = tauri::generate_context!();
+    let app = tauri::Builder::default()
+        .setup(|app| {
+            // Obtiene una referencia a la ventana principal
+            let window = app.get_window("main"); // "main" es el nombre de la ventana principal, puedes cambiarlo si es necesario
+
+            // Comprueba si la ventana se pudo obtener
+            if let Some(window) = window {
+                // Activa las sombras para la ventana (si estás utilizando window_shadows)
+                #[cfg(any(windows, target_os = "windows"))]
+                set_shadow(&window, true).unwrap();
+            }
+
+            Ok(())
+        })
+        .run(context);
+
+    if let Err(e) = app {
+        eprintln!("Error while running Tauri application: {:?}", e);
+    }
+
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
